480d9e37459d7f319f0afaab1d677a92f6c20943
Jacob-xyb <949782197@qq.com>
2021-12-08 10:56:21 +0800

    #TODO LinearAlgebra

---
+++ Code/chapter02/2.2_LinearAlgebra.cpp
@@ -0,0 +1,17 @@
+#include "../Jx_Head.h"
+
+void LinearAlgebra()
+{
+	/*
+	函数		功能
+	trace	对角线元素之和(矩阵的迹)
+	diag	对角线元素
+	triu/tril	矩阵的上三角/下三角，可指定偏移量
+	mm/bmm		矩阵乘法，batch的矩阵乘法
+	addmm/addbmm/addmv/addr/baddbmm..	矩阵运算
+	t			转置
+	dot/cross	内积/外积
+	inverse		求逆矩阵
+	svd			奇异值分解
+	*/
+}
